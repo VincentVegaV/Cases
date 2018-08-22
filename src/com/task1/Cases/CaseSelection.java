5cfd54f47ba4f9bcdc127c71743b331ae623cc5f
VincentVegaV <forgitmail@gmail.com>
2018-08-22 23:17:34 +0300

    0.1

--- src/com/task1/Cases/CaseSelection.java
+++ src/com/task1/Cases/CaseSelection.java
@@ -1,4 +1,25 @@
 package com.task1.Cases;
 
-public class CaseSelection {
+import java.util.List;
+import java.util.Random;
+
+    public class CaseSelection {
+
+        public void cutAndPaste(List<String> initialList, List<String> resultList, Integer numberCases)
+        {
+
+            Random rand = new Random();
+
+            if(initialList.size() >= numberCases)
+            {
+                for (int i = 0; i < numberCases; i++)
+                {
+                    int randomIndex = rand.nextInt(initialList.size());
+                    resultList.add(i, initialList.get(randomIndex));
+                    initialList.remove(randomIndex);
+                }
+            }else if(initialList.size() < numberCases){
+                System.out.println("Not enough cases to select. You have only " + initialList.size() + " case(s) in the file.");
+            }
+        }
 }
--- src/com/task1/Cases/InputHandler.java
+++ src/com/task1/Cases/InputHandler.java
@@ -1,4 +1,44 @@
 package com.task1.Cases;
 
-public class InputHandler {
-}
+import java.util.Scanner;
+import java.io.*;
+
+    public class InputHandler {
+        private String path = "";
+        private String number = "";
+        File fileChecker;
+
+        Scanner scanner = new Scanner(System.in);
+
+        String getPath(){
+
+            while (path.trim().equals("")){
+                System.out.println("Enter absolute Path:");
+                path = scanner.nextLine();
+            }
+            fileChecker = new File(path);
+
+            while (!fileChecker.exists()){
+                System.out.println("File doesn't exist.");
+                path = scanner.nextLine();
+                fileChecker = new File(this.path);
+            }
+
+            return path;
+        }
+
+
+        Integer getNumber(){
+            System.out.println("Enter number (Press ENTER to set default):");
+
+            number = scanner.nextLine();
+
+            if (number.trim().equals("")){
+                number = "10";
+                System.out.println("Default number is " + number +".");
+            }
+
+            return Integer.parseInt(number);
+        }
+
+    }
